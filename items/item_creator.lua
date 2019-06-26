6bca4507fee46cf92331eb109e2eca0c2eac2b33
Nicholas Cook <nickcook530@gmail.com>
2019-06-26 00:01:15 -0400

    created first enemy, when user selects option message choice to enemy and receive move in response

--- items/item_creator.lua
+++ items/item_creator.lua
@@ -3,6 +3,8 @@
 -- require "my_directory.my_file"
 -- in any script using the functions.
 
+
+-- Should eventually find a way to only generate this once then save it
 master_item_table = {}
 
 function get_max_key(t)
@@ -22,20 +24,24 @@ print(max_key_master_item_table)
 item = {}
 item.__index = item
 
-function item.new(name, type, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
-	return setmetatable({name = name or "ERROR", type = type or "ERROR", on_win = { dmg = win_dmg or 0, heal = win_heal or 0,
+function item.new(name, type, level, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
+	return setmetatable({name = name or "ERROR", type = type or "ERROR", level = level or "ERROR", on_win = { dmg = win_dmg or 0, heal = win_heal or 0,
 	effect = win_effect or {}}, on_tie = { dmg = tie_dmg or 0, heal = tie_heal or 0, effect = tie_effect or {}},
 	on_loss = { dmg = loss_dmg or 0, heal = loss_heal or 0, effect = loss_effect or {}} }, item)
 end
 
-function create_new_item(item_variable, name, type, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
-	item_variable = item.new(name, type, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
+function create_new_item(item_variable, name, type, level, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
+	item_variable = item.new(name, type, level, win_dmg, win_heal, win_effect, tie_dmg, tie_heal, tie_effect, loss_dmg, loss_heal, loss_effect)
 	master_item_table[max_key_master_item_table] = item_variable
 	max_key_master_item_table = max_key_master_item_table + 1
 	print("CREATED")
 end
 
-create_new_item(starter_sword, "Starter Sword", sword, 1, 0, nil, 0, 0, nil, 0, 0, nil)
+create_new_item(starter_shield, "Starter Shield", shield, 1, 1, 0, nil, 0, 0, nil, 0, 0, nil)
+create_new_item(starter_scroll, "Starter Scroll", sword, 1, 1, 0, nil, 0, 0, nil, 0, 0, nil)
+create_new_item(starter_sword, "Starter Sword", sword, 1, 1, 0, nil, 0, 0, nil, 0, 0, nil)
 
 print(master_item_table[1]["name"])
+print(master_item_table[2]["name"])
+print(master_item_table[3]["name"])
 
